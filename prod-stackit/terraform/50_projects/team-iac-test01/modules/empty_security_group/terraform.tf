d44bed96210f3155e57f443db765c252d6099bad
Roman Schwarz <rs@cloudeteer.de>
2025-03-14 13:16:53 +0100

    Remove openstack provider from submodules were not needed
    Signed-off-by: Roman Schwarz <rs@cloudeteer.de>

--- prod-stackit/terraform/50_projects/team-iac-test01/modules/empty_security_group/terraform.tf
+++ prod-stackit/terraform/50_projects/team-iac-test01/modules/empty_security_group/terraform.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = "~> 1.8"
 
   required_providers {
-    openstack = {
-      source  = "terraform-provider-openstack/openstack"
-      version = "~> 3.0"
-    }
     stackit = {
       source  = "stackitcloud/stackit"
       version = "~> 0.43"
