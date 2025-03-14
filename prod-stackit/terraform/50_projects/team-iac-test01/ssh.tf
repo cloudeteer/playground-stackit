bff8b6a65c3d33636b91baf57fb990a8b71c9a71
Roman Schwarz <rs@cloudeteer.de>
2025-03-14 13:10:40 +0100

    Use `local_sensitive_file` for private key
    Signed-off-by: Roman Schwarz <rs@cloudeteer.de>

--- prod-stackit/terraform/50_projects/team-iac-test01/ssh.tf
+++ prod-stackit/terraform/50_projects/team-iac-test01/ssh.tf
@@ -7,7 +7,7 @@ resource "stackit_key_pair" "default" {
   public_key = chomp(tls_private_key.default.public_key_openssh)
 }
 
-resource "local_file" "id_ed25519" {
+resource "local_sensitive_file" "id_ed25519" {
   content  = tls_private_key.default.private_key_openssh
   filename = "${path.root}/id_ed25519"
 }
