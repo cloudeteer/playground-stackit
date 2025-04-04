8c775d876ce527c16f318d048bcefd30c6dfaa40
Roman Schwarz <rs@cloudeteer.de>
2025-04-04 12:23:17 +0200

    Manage project `team-iac-test01` as Terraform resource
    Signed-off-by: Roman Schwarz <rs@cloudeteer.de>

--- prod-stackit/terraform/50_projects/team-iac-test01/data.tf
+++ prod-stackit/terraform/50_projects/team-iac-test01/data.tf
@@ -1,5 +1,4 @@
-data "stackit_resourcemanager_project" "this" {
-  count = local.any_feature_enabled ? 1 : 0
-
-  project_id = "341539db-8c67-43cf-ba1f-fd14157a0a5b"
+data "stackit_network_area" "this" {
+  organization_id = "6f0a04e7-8d5f-42a5-b1b3-89b8532126db" # CLOUDETEER GmbH
+  network_area_id = "9ee7d3fd-66de-499e-9687-1aaac356afc4" # hub-and-spoke-01
 }
--- prod-stackit/terraform/50_projects/team-iac-test01/main.tf
+++ prod-stackit/terraform/50_projects/team-iac-test01/main.tf
@@ -1,4 +1,21 @@
 #
+# STACKIT Project
+#
+
+resource "stackit_resourcemanager_project" "this" {
+  count = local.any_feature_enabled ? 1 : 0
+
+  name                = "team-iac-test01"
+  parent_container_id = "965b1adb-f3f9-4f46-9ee4-bc05682afe72" # Org: CLOUDETEER GmbH / Folder: Chapter OE
+  owner_email         = jsondecode(var.stackit_service_account_key)["credentials"]["iss"]
+
+  labels = {
+    "ephemeral"   = "true"
+    "networkArea" = data.stackit_network_area.this.network_area_id
+  }
+}
+
+#
 # DNS
 #
 
@@ -8,7 +25,7 @@ module "dns" {
   count = var.features.dns ? 1 : 0
 
   labels       = local.labels
-  project_id   = one(data.stackit_resourcemanager_project.this[*].project_id)
+  project_id   = one(stackit_resourcemanager_project.this[*].project_id)
   keypair_name = one(stackit_key_pair.default[*].name)
 
   debug = false
@@ -24,7 +41,7 @@ module "empty_security_group" {
   count = var.features.empty_security_group ? 1 : 0
 
   labels     = local.labels
-  project_id = one(data.stackit_resourcemanager_project.this[*].project_id)
+  project_id = one(stackit_resourcemanager_project.this[*].project_id)
 }
 
 #
@@ -38,7 +55,7 @@ module "firewall" {
 
   ipv4_nameservers = length(module.dns) == 1 ? module.dns[0].ipv4_nameservers : local.fallback_nameserver
   labels           = local.labels
-  project_id       = one(data.stackit_resourcemanager_project.this[*].project_id)
+  project_id       = one(stackit_resourcemanager_project.this[*].project_id)
 }
 
 #
@@ -53,7 +70,7 @@ module "load_balancer" {
   ipv4_nameservers = length(module.dns) == 1 ? module.dns[0].ipv4_nameservers : local.fallback_nameserver
   keypair_name     = one(stackit_key_pair.default[*].name)
   labels           = local.labels
-  project_id       = one(data.stackit_resourcemanager_project.this[*].project_id)
+  project_id       = one(stackit_resourcemanager_project.this[*].project_id)
 
   backend_server_count                   = 3
   backend_server_backup_schedule_enabled = false
